312d7b19801fe21c563434f6d0c0a1f4cd36185e
Jiya Garg <100217150+Jiya-garg9971@users.noreply.github.com>
2022-11-10 17:27:12 +0530

    Birthday Calender
    Created using React Js.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <h1>The Birthday Calender</h1>
+  <App/>
+  </>
+);
+
+
